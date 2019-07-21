5e642b5771433f7acd135890c48b59253c31ddea
musawwer <musawer@gmail.com>
2019-07-21 16:58:13 +0500

    First commit

---
+++ app/Http/Controllers/testing.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testing extends Controller
+{
+    
+
+
+    public function testing(){
+
+
+        return view('testing');
+    }
+}
